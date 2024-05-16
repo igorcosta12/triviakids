0ad976bdeb0009b44ba42e60282a1fa027eaaf97
Igor Costa <89418144+igorcosta12@users.noreply.github.com>
2024-05-16 15:07:25 -0300

    Add files via upload

---
+++ quizpage.js
@@ -0,0 +1,72 @@
+let perguntas = [];
+let quantidadeCertas = 0;
+let perguntasRespondidas = 0;
+
+async function carregarPerguntas() {
+    try {
+        const response = await fetch('perguntas.txt');
+        perguntas = await response.json();
+        gerarPerguntas();
+    } catch (error) {
+        console.error('Erro ao carregar as perguntas:', error);
+    }
+}
+
+function obterTema() {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get('tema');
+}
+
+function gerarPerguntas() {
+    const tema = obterTema();
+    const gradePerguntas = document.querySelector('.question-grid');
+    const perguntasFiltradas = perguntas.filter(pergunta => pergunta.tema === tema);
+
+    perguntasFiltradas.forEach((pergunta, indice) => {
+        const cartaoPergunta = document.createElement('div');
+        cartaoPergunta.className = 'cartao-pergunta';
+        cartaoPergunta.innerHTML = `
+            <h2>Pergunta ${indice + 1}:</h2>
+            <p>${pergunta.pergunta}</p>
+            <ul class="lista-respostas">
+                ${pergunta.respostas.map((resposta, i) => `<li><button data-resposta="${resposta}" onclick="verificarResposta(this, ${indice})">${resposta}</button></li>`).join('')}
+            </ul>
+        `;
+        gradePerguntas.appendChild(cartaoPergunta);
+    });
+}
+
+function verificarResposta(botao, indicePergunta) {
+    const tema = obterTema();
+    const perguntasFiltradas = perguntas.filter(pergunta => pergunta.tema === tema);
+    const respostaCerta = perguntasFiltradas[indicePergunta].correta;
+    const respostaUsuario = botao.getAttribute('data-resposta');
+    const estaCorreta = respostaUsuario === respostaCerta;
+
+    const todasRespostas = botao.closest('.cartao-pergunta').querySelectorAll("button");
+
+    todasRespostas.forEach(function(resposta) {
+        resposta.disabled = true;
+        resposta.classList.remove("correta", "incorreta");
+        if (resposta.getAttribute('data-resposta') === respostaCerta) {
+            resposta.classList.add("correta");
+        } else {
+            resposta.classList.add("incorreta");
+        }
+    });
+
+    if (estaCorreta) {
+        quantidadeCertas++;
+    }
+
+    perguntasRespondidas++;
+    atualizarPlacar();
+}
+
+function atualizarPlacar() {
+    document.getElementById('quantidade-certas').innerText = quantidadeCertas;
+    document.getElementById('quantidade-total').innerText = perguntas.filter(pergunta => pergunta.tema === obterTema()).length;
+    document.getElementById('resultado').style.display = 'block';
+}
+
+carregarPerguntas();
