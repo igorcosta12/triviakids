b5d8c9d2d3ce7b412c20fd63211154f6a7d13120
Igor Costa <89418144+igorcosta12@users.noreply.github.com>
2024-05-22 17:24:57 -0300

    Update quizpage.js

--- quizpage.js
+++ quizpage.js
@@ -1,41 +1,3 @@
-let perguntas = [];
-let quantidadeCertas = 0;
-let perguntasRespondidas = 0;
-
-async function carregarPerguntas() {
-    try {
-        const response = await fetch('perguntas.txt');
-        perguntas = await response.json();
-        gerarPerguntas();
-    } catch (error) {
-        console.error('Erro ao carregar as perguntas:', error);
-    }
-}
-
-function obterTema() {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get('tema');
-}
-
-function gerarPerguntas() {
-    const tema = obterTema();
-    const gradePerguntas = document.querySelector('.question-grid');
-    const perguntasFiltradas = perguntas.filter(pergunta => pergunta.tema === tema);
-
-    perguntasFiltradas.forEach((pergunta, indice) => {
-        const cartaoPergunta = document.createElement('div');
-        cartaoPergunta.className = 'cartao-pergunta';
-        cartaoPergunta.innerHTML = `
-            <h2>Pergunta ${indice + 1}:</h2>
-            <p>${pergunta.pergunta}</p>
-            <ul class="lista-respostas">
-                ${pergunta.respostas.map((resposta, i) => `<li><button data-resposta="${resposta}" onclick="verificarResposta(this, ${indice})">${resposta}</button></li>`).join('')}
-            </ul>
-        `;
-        gradePerguntas.appendChild(cartaoPergunta);
-    });
-}
-
 function verificarResposta(botao, indicePergunta) {
     const tema = obterTema();
     const perguntasFiltradas = perguntas.filter(pergunta => pergunta.tema === tema);
@@ -50,7 +12,7 @@ function verificarResposta(botao, indicePergunta) {
         resposta.classList.remove("correta", "incorreta");
         if (resposta.getAttribute('data-resposta') === respostaCerta) {
             resposta.classList.add("correta");
-        } else if (resposta.getAttribute('data-resposta') === respostaUsuario) {
+        } else if (resposta.getAttribute('data-resposta') === respostaUsuario && !estaCorreta) {
             resposta.classList.add("incorreta");
         }
     });
@@ -65,15 +27,3 @@ function verificarResposta(botao, indicePergunta) {
     perguntasRespondidas++;
     atualizarPlacar();
 }
-
-function atualizarPlacar() {
-    const tema = obterTema();
-    const totalPerguntas = perguntas.filter(pergunta => pergunta.tema === tema).length;
-    if (perguntasRespondidas === totalPerguntas) {
-        document.getElementById('quantidade-certas').innerText = quantidadeCertas;
-        document.getElementById('quantidade-total').innerText = totalPerguntas;
-        document.getElementById('resultado').style.display = 'block';
-    }
-}
-
-carregarPerguntas();
